643d676966b63dda4ba8b64bef4d909bd3a48a51
rokuden <rokuden@sfc.wide.ad.jp>
2016-07-28 20:42:57 +0900

    Create roomba_rr.py

---
+++ roomba_rr.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+
+
+import serial
+import time
+
+
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout = 1)
+
+
+ser.write(chr(128))#start
+ser.write(chr(131))#safe mode
+ser.write(chr(146))#drive PWM
+ser.write(chr(255))#R PWM HIGH
+ser.write(chr(1))#R PWM LOW
+ser.write(chr(0))#L PWM HIGH
+ser.write(chr(255))#L PWM LOW
+
+time.sleep(0.35)
+
+ser.write(chr(146))
+ser.write(chr(0))
+ser.write(chr(0))
+ser.write(chr(0))
+ser.write(chr(0))
+ser.close()
